1a31888837cae749e2a525903217c4a3efcfd87f
dlbrptrvc <37641298+dlbrptrvc@users.noreply.github.com>
2023-09-30 21:55:13 +0200

    Fonts, styles and images added

--- script.js
+++ script.js
@@ -5,18 +5,23 @@ function initializeDOM(parentElementID) {
     });
 }
 
-initializeDOM('main')
+initializeDOM('body')
 let activeBrush = 'color'
 let currentColor = 'rgb(0,0,0)'
 let brushOn = false
+
+boardColor.value = "#FFFFFF"
+boardSize.value = '16'
 document.addEventListener('mouseup', e => brushOn = false)
 
 function initializeBoard(x) {
+    let hex = boardColor.value
+    let rgb = 'rgb('+parseInt(hex.slice(1, 3), 16)+','+parseInt(hex.slice(3, 5), 16)+','+parseInt(hex.slice(5, 7), 16)+')'
     for (let i=0;i<x;i++) {
         let row = document.createElement('row')
         for (let i=0;i<x;i++){
             let pixel = document.createElement('pixel')
-            pixel.style.backgroundColor = 'rgb(255,255,255)'
+            pixel.style.backgroundColor = rgb
             pixel.addEventListener('mousedown', e => {brushOn = true;paint(e.target)})
             pixel.addEventListener("mouseover", e => paint(e.target))
             pixel.addEventListener("dragstart", e => e.preventDefault())
@@ -29,8 +34,14 @@ function initializeBoard(x) {
 initializeBoard(boardSize.value)
 
 Array.from(document.querySelectorAll(".brush")).forEach(element =>
-    element.addEventListener('click',()=>activeBrush=element.id))
-    
+    element.addEventListener('click',(e)=>{toggleHighlight(e.target);activeBrush=element.id}))
+
+function toggleHighlight(btn) {
+    window[activeBrush].className = window[activeBrush].className.replace(' highlighted','')
+    btn.className += ' highlighted'
+
+}
+
 function paint(target) {
     if (brushOn == true) {
         let rgb = target.style.backgroundColor.replace(/rgb\(| |\)/g, '').split(',')
@@ -68,7 +79,7 @@ function clearBoard() {
 
 boardSize.addEventListener('input', ()=>{
     let x = boardSize.value
-    boardSizeLabel.textContent = 'Size: '+x+'x'+x
+    boardSizeLabel.textContent = 'Size '+x+'x'+x
 })
 
 changeBoardSize.addEventListener('click', clearBoard)
@@ -77,3 +88,37 @@ colorSelector.addEventListener('input',()=>{
     let hex = colorSelector.value
     currentColor='rgb('+parseInt(hex.slice(1, 3), 16)+','+parseInt(hex.slice(3, 5), 16)+','+parseInt(hex.slice(5, 7), 16)+')'
 })
+
+setInterval(backgroundPix,1000)
+let blockCounter = 0
+
+function backgroundPix() {
+    let blockWidth=Math.floor(Math.random()*500)
+    let blockPositionX=Math.floor(Math.random()*window.innerWidth)
+    let blockPositionY=Math.floor(Math.random()*window.innerHeight)
+    if (window['block'+blockCounter]) {
+        Object.assign(window['block'+blockCounter].style, {
+            backgroundColor : 'rgba('+Math.floor(Math.random()*255)+','+Math.floor(Math.random()*255)+','+Math.floor(Math.random()*255)+',0.2)',
+            left: blockPositionX-Math.floor(blockWidth/2)+'px',
+            top: blockPositionY-Math.floor(blockWidth/2)+'px',
+            width: blockWidth+'px',
+            height: blockWidth+'px',
+        })
+    } else {
+        window['block'+blockCounter]=document.createElement("block")
+        Object.assign(window['block'+blockCounter].style, {
+            backgroundColor : 'rgba('+Math.floor(Math.random()*255)+','+Math.floor(Math.random()*255)+','+Math.floor(Math.random()*255)+',0.2)',
+            left: blockPositionX-Math.floor(blockWidth/2)+'px',
+            top: blockPositionY-Math.floor(blockWidth/2)+'px',
+            width: blockWidth+'px',
+            height: blockWidth+'px',
+        })
+        body.append(window['block'+blockCounter])
+    }
+    if (blockCounter<39){blockCounter++}
+    else blockCounter=0
+}
+
+for (let i=0;i<40;i++) {
+    backgroundPix()
+}
